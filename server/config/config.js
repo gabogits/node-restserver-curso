baabff425159cdd4f4f6241be166fb7478b93dfc
Gabriel <gabriel@kubofinanciero.com>
2018-09-19 19:19:00 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abarcafe1@ds263642.mlab.com:63642/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 
